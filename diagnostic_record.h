e7a1e85aa53e169d1b06a56b98ebcdf7f18e3202
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-06 21:19:44 +0200

    Add constructors and display method to DiagnosticRecord
    Introduced two constructors to the `DiagnosticRecord` class, allowing initialization with either a string or a `Date` object for the date. Implemented the `displayRecord` method to override the base class method, providing formatted output of the diagnosis record's date and details.

--- diagnostic_record.h
+++ diagnostic_record.h
@@ -4,7 +4,14 @@
 #include "medical_record.h"
 
 class DiagnosticRecord : public MedicalRecord {
+public:
+	DiagnosticRecord(const string& d, const string& det) : MedicalRecord(d, det) {}//DiagnosticRecord based on MedicalRecord
 
+    DiagnosticRecord(const Date d, const string& det) : MedicalRecord(d, det) {}
+
+    void displayRecord() const override {
+        cout << "Diagnosis Record - Date: " << _date.toString() << ", Details: " << _details << endl;
+    }
 };
 #endif // !DIAGNOSTIC_RECORD
 
