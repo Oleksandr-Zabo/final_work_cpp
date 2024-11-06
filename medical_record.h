9cb2a61b3e078d618ed3f4cf3df5a10e34cf0512
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-06 21:13:20 +0200

    Add Date struct and update MedicalRecord class
    - Added `#include <sstream>` to `lib.h` for string stream operations.
    - Introduced `Date` struct in `medical_record.h` with private members `_day`, `_month`, and `_year`, and public methods for access and string conversion.
    - Updated `MedicalRecord` class to include `Date` member `_date` and string member `_details`.
    - Added constructors to `MedicalRecord` to initialize `_date` and `_details`.
    - Made `MedicalRecord` an abstract class by adding pure virtual method `displayRecord()`.

--- medical_record.h
+++ medical_record.h
@@ -4,8 +4,71 @@
 #include "lib.h"
 #include "IMedicalRecord.h"
 
-class MedicalRecord : public IMedicalRecord {
+struct Date {
+private:
+    int _day;
+    int _month;
+    int _year;
+public:
+	Date(){
+		_day = 0;
+		_month = 0;
+		_year = 0;
+	}
+	Date(int day, int month, int year) : _day(day), _month(month), _year(year) {
+		_day = day;
+		_month = month;
+		_year = year;
+	}
+
+	int getDay() const { return _day; }
+
+	int getMonth() const { return _month; }
+
+	int getYear() const { return _year; }
+
+	void setDay(int day) { _day = day; }
+
+	void setMonth(int month) { _month = month; }
+
+	void setYear(int year) { _year = year; }
+
+	string toString() const {
+		return to_string(_day) + "/" + to_string(_month) + "/" + to_string(_year);
+	}
+
+	static Date fromString(const string& str) {
+		Date date;
+		stringstream ss(str);
+		string token;
+		vector<string> tokens;
+		while (getline(ss, token, '/')) {
+			tokens.push_back(token);
+		}
+		date.setDay(stoi(tokens[0]));
+		date.setMonth(stoi(tokens[1]));
+		date.setYear(stoi(tokens[2]));
+		return date;
+	}
 
 };
-#endif // !MEDICAL_RECORD_CLASS
 
+class MedicalRecord : public IMedicalRecord {
+protected:
+    Date _date;
+    string _details;
+
+public:
+    MedicalRecord(const string& d, const string& det){
+		_date = Date::fromString(d);
+		_details = det;
+	}
+
+	MedicalRecord(const Date d, const string& det) {
+		_date = d;
+		_details = det;
+	}
+
+    virtual void displayRecord() const override = 0;
+};
+#endif // !MEDICAL_RECORD_CLASS
