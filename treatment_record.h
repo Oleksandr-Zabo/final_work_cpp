91ed8919fa7ee51577a0e46a31cd8fa0707e9757
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-09 12:29:39 +0200

    Add constructors and display methods to record classes
    The `TreatmentRecord` class now includes two constructors: one that
    accepts a `string` for the date and another that takes a `Date`
    object. Additionally, a `displayRecord` method has been added to
    output the record details.
    
    Similarly, the `VisitRecord` class has been updated to include two
    constructors for different date types and a `displayRecord` method
    to print the record's date and details.

--- treatment_record.h
+++ treatment_record.h
@@ -4,7 +4,13 @@
 #include "medical_record.h"
 
 class TreatmentRecord : public MedicalRecord {
+public:
+	TreatmentRecord(const string& d, const string& det) : MedicalRecord(d, det) {}//TreatmentRecord based on MedicalRecord
 
+	TreatmentRecord(const Date d, const string& det) : MedicalRecord(d, det) {}
+
+	void displayRecord() const override {
+		cout << "Treatment Record - Date: " << _date.toString() << ", Details: " << _details << endl;
+	}
 };
 #endif // !TREATMENT_RECORD
-
--- visit_record.h
+++ visit_record.h
@@ -4,7 +4,13 @@
 #include "medical_record.h"
 
 class VisitRecord : public IMedicalRecord {
+public:
+	VisitRecord(const string& d, const string& det) : IMedicalRecord(d, det) {}//VisitRecord based on MedicalRecord
 
+	VisitRecord(const Date d, const string& det) : IMedicalRecord(d, det) {}
+
+	void displayRecord() const override {
+		cout << "Visit Record - Date: " << _date.toString() << ", Details: " << _details << endl;
+	}
 };
 #endif // !VISIT_RECORD
-
