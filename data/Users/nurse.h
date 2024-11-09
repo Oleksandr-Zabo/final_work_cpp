649dcc0167fa16192088391467ea170cbf47b6b7
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-09 13:32:42 +0200

    Add new User and role-specific classes
    Update  `User` class in `user.h` inheriting from `IUser`, with private members `username` and `password`, and methods to manage them. Introduced `Admin`, `AdminStaff`, `Doctor`, and `Nurse` classes in their respective headers, each inheriting from `User` and including role-specific attributes and methods.

--- data/Users/nurse.h
+++ data/Users/nurse.h
@@ -4,7 +4,39 @@
 #include "user.h"
 
 class Nurse : public User {
+private:
+    string department;  // Stores the nurse's department
 
+public:
+    // Constructor
+    Nurse(const string& uname, const string& pwd, const string& dept)
+        : User(uname, pwd), department(dept) {}
+
+    // Method to display the nurse's role
+    void displayRole() const override {
+        cout << "Role: Nurse" << endl;
+    }
+
+    // Method to get the nurse's department
+    string getDepartment() const {
+        return department;
+    }
+
+    // Method to set the nurse's department
+    void setDepartment(const string& dept) {
+        department = dept;
+    }
+
+    // Method to assist doctors (example function)
+    void assistDoctor() const {
+        cout << "Nurse assisting doctor" << endl;
+    }
+
+    // Method to monitor patient vitals (example function)
+    void monitorVitals() const {
+        cout << "Nurse monitoring patient vitals" << endl;
+    }
 };
+
 #endif // !NURSE
 
--- data/Users/user.h
+++ data/Users/user.h
@@ -5,6 +5,36 @@
 #include "../Interfaces/IUser.h"
 
 class User : public IUser {
+private:
+    string username;  // Stores the username
+    string password;  // Stores the password
 
+public:
+    // Constructor
+    User(const string& uname, const string& pwd) : username(uname), password(pwd) {}
+
+    // Method to display the user's role
+	void virtual displayRole() const override = 0;
+
+    // Method to get the username
+    string getUsername() const {
+        return username;
+    }
+
+    // Method to set the username
+    void setUsername(const string& uname) {
+        username = uname;
+    }
+
+    // Method to get the password
+    string getPassword() const {
+        return password;
+    }
+
+    // Method to set the password
+    void setPassword(const string& pwd) {
+        password = pwd;
+    }
 };
+
 #endif // !USER_CLASS
