3d3780c37b70fc82889e39acd96e8dda2f6cc730
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-22 20:47:03 +0200

    Add InsurancePolicy struct and update MedicalRecord class
    Added InsurancePolicy struct with necessary attributes and methods.
    Updated MedicalRecord class to include new attributes for patient
    details and insurance policy. Added default, parameterized, and copy
    constructors for MedicalRecord. Updated displayRecord method and added
    methods to convert MedicalRecord to/from string. Removed old attributes
    and constructors. Changed displayRecord from pure virtual to concrete.

--- domain/data/MedicalRecord/medical_record.h
+++ domain/data/MedicalRecord/medical_record.h
@@ -53,22 +53,129 @@ public:
 
 };
 
+struct InsurancePolicy {
+    string policyNumber;
+    string holderName;
+    string holderAddress;
+    string insuranceCompany;
+    double coverageAmount;
+    Date startDate;
+    Date endDate;
+
+    // Default constructor
+    InsurancePolicy() {
+        policyNumber = "";
+        holderName = "";
+        holderAddress = "";
+        insuranceCompany = "";
+        coverageAmount = 0.0;
+        startDate = Date();
+        endDate = Date();
+    }
+
+    // Parameterized constructor
+    InsurancePolicy(string policyNum, string holderNm, string holderAddr, string insuranceComp, double coverageAmt, Date startDt, Date endDt) {
+        policyNumber = policyNum;
+        holderName = holderNm;
+        holderAddress = holderAddr;
+        insuranceCompany = insuranceComp;
+        coverageAmount = coverageAmt;
+        startDate = startDt;
+        endDate = endDt;
+    }
+
+    string toString() const {
+        return "Policy Number: " + policyNumber + "\n" +
+            "Holder Name: " + holderName + "\n" +
+            "Holder Address: " + holderAddress + "\n" +
+            "Insurance Company: " + insuranceCompany + "\n" +
+            "Coverage Amount: " + to_string(coverageAmount) + "\n" +
+            "Start Date: " + startDate.toString() + "\n" +
+            "End Date: " + endDate.toString();
+    }
+
+    void fromString(const string& str) {
+        istringstream iss(str);
+        string line;
+
+        getline(iss, line);
+        policyNumber = line.substr(line.find(": ") + 2);
+
+        getline(iss, line);
+        holderName = line.substr(line.find(": ") + 2);
+
+        getline(iss, line);
+        holderAddress = line.substr(line.find(": ") + 2);
+
+        getline(iss, line);
+        insuranceCompany = line.substr(line.find(": ") + 2);
+
+        getline(iss, line);
+        coverageAmount = stod(line.substr(line.find(": ") + 2));
+
+        getline(iss, line);
+        startDate = Date::fromString(line.substr(line.find(": ") + 2));
+
+        getline(iss, line);
+        endDate = Date::fromString(line.substr(line.find(": ") + 2));
+    }
+};
+
 class MedicalRecord : public IMedicalRecord {
 protected:
-    Date _date;
-    string _details;
-
+	string _name_of_patient;
+	string _surname_of_patient;
+    Date _date_of_birth;
+	InsurancePolicy _policy;
 public:
-    MedicalRecord(const string& d, const string& det){
-		_date = Date::fromString(d);
-		_details = det;
+	MedicalRecord() {
+		_name_of_patient = "";
+		_surname_of_patient = "";
+		_date_of_birth = Date();
+		_policy = InsurancePolicy();
 	}
 
-	MedicalRecord(const Date d, const string& det) {
-		_date = d;
-		_details = det;
+	MedicalRecord(string& name, string& surname, Date date_of_birth, InsurancePolicy policy){
+		_name_of_patient = name;
+		_surname_of_patient = surname;
+		_date_of_birth = date_of_birth;
+		_policy = policy;
+
+    }
+
+	MedicalRecord(const MedicalRecord& medical_record) {
+		_name_of_patient = medical_record._name_of_patient;
+		_surname_of_patient = medical_record._surname_of_patient;
+		_date_of_birth = medical_record._date_of_birth;
+		_policy = medical_record._policy;
 	}
 
-    virtual void displayRecord() const override = 0;
+    void displayRecord() const override {
+        cout << "Name of patienn: " << _name_of_patient << endl;
+		cout << "Surname of patient: " << _surname_of_patient << endl;
+		cout << "Date of birth: " << _date_of_birth.toString() << endl;
+		cout << "Insurance policy: " << _policy.toString() << endl;
+    }
+
+	string Medical_Record_to_string() const {
+		return "Name of patient: " + _name_of_patient + "\n" +
+			"Surname of patient: " + _surname_of_patient + "\n" +
+			"Date of birth: " + _date_of_birth.toString() + "\n" +
+			"Insurance policy: " + _policy.toString();
+	}
+
+	void Medical_Record_from_string(const string& str) {
+		istringstream iss(str);
+		string line;
+		getline(iss, line);
+		_name_of_patient = line.substr(line.find(": ") + 2);
+		getline(iss, line);
+		_surname_of_patient = line.substr(line.find(": ") + 2);
+		getline(iss, line);
+		_date_of_birth = Date::fromString(line.substr(line.find(": ") + 2));
+		getline(iss, line);
+		_policy.fromString(line.substr(line.find(": ") + 2));
+	}
+
 };
 #endif // !MEDICAL_RECORD_CLASS
